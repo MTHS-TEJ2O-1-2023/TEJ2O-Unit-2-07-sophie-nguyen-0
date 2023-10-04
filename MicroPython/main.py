9ef8dff7bceda2a32238f79875b7f7f3724439ca
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-04 16:52:04 +0000

    py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Sophie
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HEART)
 
-display.scroll("Hello, World!")
+# variable
+number_of_clicks = 0
+
+while True:
+    if button_a.is_pressed():
+        number_of_clicks = number_of_clicks + 1
+        display.show(str(number_of_clicks))
+    if button_b.is_pressed():
+        number_of_clicks = 0
+        display.show(str(number_of_clicks))
